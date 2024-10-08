fdff891a921442bce5f1f522289bd10005259d8c
Festongithub <fkemboik@gmail.com>
2024-10-08 11:56:00 +0000

    Redis initialised

---
+++ 0x03-queuing_system_in_js/0-redis_client.js
@@ -0,0 +1,11 @@
+import redis from 'redis';
+
+const client = redis.createClient();
+
+client.on('connect', () => {
+	console.log(`Redis client connected to the server`);
+});
+
+cliennt.on('error', err => {
+	console.log(`Redis client not connected to the server: ${err}`);
+});
